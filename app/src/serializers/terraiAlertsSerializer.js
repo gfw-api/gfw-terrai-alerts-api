3a8f15d21832f7394cede86e718bc6962aaa66e4
rrequero <rareq1987@gmail.com>
2016-05-31 18:09:18 +0200

    add latest and ::date in query

--- app/src/serializers/terraiAlertsSerializer.js
+++ app/src/serializers/terraiAlertsSerializer.js
@@ -3,20 +3,30 @@
 var logger = require('logger');
 var JSONAPISerializer = require('jsonapi-serializer').Serializer;
 var terraiAlertsSerializer = new JSONAPISerializer('terrai-alerts', {
-    attributes: ['value', 'period', 'min_date', 'max_date','downloadUrls'],
-    typeForAttribute: function (attribute, record) {
+    attributes: ['value', 'period', 'min_date', 'max_date', 'downloadUrls'],
+    typeForAttribute: function(attribute, record) {
         return attribute;
     },
-    downloadUrls:{
+    downloadUrls: {
         attributes: ['csv', 'geojson', 'kml', 'shp', 'svg']
+    }
+});
+
+var terraiAlertsLatestSerializer = new JSONAPISerializer('terrai-latest', {
+    attributes: ['grid_code', 'date'],
+    typeForAttribute: function(attribute, record) {
+        return attribute;
     }
 });
 
 class TerraiAlertsSerializer {
 
-  static serialize(data) {
-    return terraiAlertsSerializer.serialize(data);
-  }
+    static serialize(data) {
+        return terraiAlertsSerializer.serialize(data);
+    }
+    static serializeLatest(data) {
+        return terraiAlertsLatestSerializer.serialize(data);
+    }
 }
 
 module.exports = TerraiAlertsSerializer;
